9d3f5a20884a19655f3dcc44ac84b7d9843b4dab
Ngras <nicolas.g33@hotmail.fr>
2022-06-15 11:11:49 +0200

    fetchSlide template

--- src/Template/TemplateHandler.php
+++ src/Template/TemplateHandler.php
@@ -12,13 +12,26 @@ class TemplateHandler extends SlideHandler
         $templateAccessKey = $this->needed_templates();
 
         if (is_array($templateAccessKey)) {
-            $template = $slide->getTemplate();
+            $templateData = $slide->getTemplate();
+            $template = $templateData["template"];
+            $amplitude = $templateData["amplitude"];
+            $objectsArray = $templateData["objectsArray"];
+
+            $datasource = null;
+            $sources = [];
+
+            foreach ($template["objects"] as $obj) {
+                if (array_key_exists('text', $obj['properties']) && $obj['properties']['text']['__type'] == 'datasource') {
+                    array_push($sources, $obj['properties']['text']['datasource']);
+                }
+            }
+
+            foreach ($objectsArray as $objects) {
+                $this->addSlide([
+                    "canvas" => $template["canvas"],
+                    "objects" => $objects
+                ]);
+            }
         }
-
-        dd($template);
-
-        $this->addSlide([
-            "template" => $template
-        ]);
     }
 }
